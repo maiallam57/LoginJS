ee79ebf03856dfc82a0a49a16f95c1642efaf926
maiallam57 <maimallam57@gmail.com>
2024-07-07 15:38:58 +0300

    allDone

--- js/script.js
+++ js/script.js
@@ -7,28 +7,31 @@ const signupPassword = document.getElementById("signupPassword");
 
 const incorrectMsg = document.getElementById("incorrect");
 
+const gotoSignUp = document.getElementById("goto-sign-up")
+const gotoLogin = document.getElementById("goto-login")
+
+
 var users = []
-var correct = false;
+var userInfo;
+var correctSignUp = false;
 
 let regex = {
-    name: {
-        value: /^[a-z]{3,15}$/,
+    signupName: {
+        value: /^[a-z\s]{3,15}$/,
         invalidMsg: "Name must begin with char and between 3 to 15 letter!"
     },
-    email: {
+    signupEmail: {
         value: /^[a-zA-Z0-9.!#_-]+@(yahoo|google|gmail|skype|hotmail|outlook)\.com$/,
         invalidEmailMsg: "Invalid Email!",
         emailDoesNotExistMsg: "This Email Doesn't Exist!",
         emailExistMsg:"This Email Already has an Account!"
     },
-    password: {
-        value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    signupPassword: {
+        value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
         invalidPasswordMsg: "Invalid Password! The password must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters",
         wrongPasswordMsg: "Sorry, wrong Password!"
     }
 }
-
-
 
 if (localStorage.getItem('users') == null) {
     users = []
@@ -36,69 +39,14 @@ if (localStorage.getItem('users') == null) {
     users = JSON.parse(localStorage.getItem('users'))
 }
 
-
 var username = sessionStorage.getItem('sessionUsername')
+
 if (username) {
     welcomePage(username)
 }
 
 function updateLocalStorage(){
-    localStorage.setItem("user", JSON.stringify(user));
-}
-
-function loginPage(){
-    document.querySelector("body").innerHTML = `
-    <div class="container my-5 text-center">
-        <div
-            class="card mb-3 bg-main-color text-white p-5 w-75 m-auto rounded-0 box-shadow">
-            <h1>Smart Login System</h1>
-            <div class="card-body">
-                <input class="form-control mb-3 text-white" type="email"
-                    id="email" placeholder="Enter your Email">
-                <input class="form-control mb-3 text-white" type="password"
-                    id="password" placeholder="Enter your Password">
-                <p id="incorrect"></p>
-
-                <button type="button" onclick="login()"
-                    class="btn btn-outline-info w-100 mb-3">Login</button>
-
-                <p class="text-white">Don’t have an account? <a
-                        class="text-white" href="signup.html">Sign
-                        Up</a>
-                </p>
-            </div>
-        </div>
-    </div>
-    `
-}
-
-function SignUpPage(){
-    document.querySelector("body").innerHTML = `
-    <div class="container my-5 text-center">
-        <div
-            class="card mb-3 bg-main-color text-white p-5 w-75 m-auto rounded-0 box-shadow">
-            <h1>Smart Login System</h1>
-            <div class="card-body">
-                <input class="form-control mb-3 text-white" type="text"
-                    id="name" placeholder="Enter your Name">
-                <input class="form-control mb-3 text-white" type="email"
-                    id="email" placeholder="Enter your Email">
-                <input class="form-control mb-3 text-white" type="password"
-                    id="password" placeholder="Enter your Password">
-
-                <p id="incorrect"></p>
-
-                <button type="button" onclick="signup()"
-                    class="btn btn-outline-info w-100  mb-3">Sign
-                    Up</button>
-
-                <p class="text-white">Don’t have an account? <a
-                        class="text-white" href="/">Login</a>
-                </p>
-            </div>
-        </div>
-    </div>
-    `
+    localStorage.setItem("users", JSON.stringify(users));
 }
 
 function welcomePage(userName){
@@ -120,7 +68,7 @@ function welcomePage(userName){
                     <ul class="navbar-nav ms-auto border-1 border-warning">
                         <li class="nav-item">
                             <a class="nav-link btn btn-outline-warning"
-                                href="/" onclick="logout()">Logout</a>
+                                href="index.html" onclick="logout()">Logout</a>
                         </li>
 
                     </ul>
@@ -138,116 +86,128 @@ function welcomePage(userName){
     `
 }
 
-function clearlogin(){
-    loginEmail.value = null;
-    loginPassword.value = null;
-}
-
-function clearSignUp(){
+function clearForm(){
     signupName.value = null;
     signupEmail.value = null;
     signupPassword.value = null;
+    loginEmail.value = null;
+    loginPassword.value = null;
+    
 }
 
 function successClass(msg){
     incorrectMsg.classList.add("text-success");
     incorrectMsg.classList.remove("text-danger");
     incorrectMsg.classList.remove("text-warning");
+    incorrectMsg.classList.remove("d-none");
     incorrectMsg.innerText= msg
 }
 function dangerClass(msg){
     incorrectMsg.classList.remove("text-success");
     incorrectMsg.classList.add("text-danger");
     incorrectMsg.classList.remove("text-warning");
+    incorrectMsg.classList.remove("d-none");
     incorrectMsg.innerText= msg
 }
 function warningClass(msg){
     incorrectMsg.classList.remove("text-success");
     incorrectMsg.classList.remove("text-danger");
     incorrectMsg.classList.add("text-warning");
+    incorrectMsg.classList.remove("d-none");
     incorrectMsg.innerText= msg
 }
 
-
 function checkUserEmailInDatabase(email){
-    var searchedUser = {}
     for (let index = 0; index < users.length; index++) {
-        item = users[index].email;
-        if (item.toLowerCase().includes(email.toLowerCase())) {
-            searchedUser = users[index]
-            break
+        userEmail = users[index].email;
+        if (email.toLowerCase() == userEmail.toLowerCase()) {
+            correctSignUp = false;
+            userInfo = users[index]
+            return
         }
     }
-    return searchedUser
 }
 
 function checkUserPasswordInDb(user){
-    if(loginPassword.value == user.password){
-        console.log("right Password");
-        return true;
-    }else{
-        console.log("wrong Password");
-        return false;
-    }
+    return loginPassword.value == user.password? true : false
 }
 
-
 function signup(){
-    if(checkUserEmailInDatabase(signupEmail.value)!= undefined){
-        console.log(regex.semail.emailExistMsg);
-        warningClass(regex.email.emailExistMsg)
+    let name = signupName.value;
+    let email = signupEmail.value;
+    let password = signupPassword.value;
+
+    if(name == "" | !regex.signupName.value.test(name)){
+        dangerClass(regex.signupName.invalidMsg)
+        return
     }
-    if(correct){
-        var user = {
-            id: users.length,
-            name: signupName.value,
-            email: signupEmail.value,
-            password:signupPassword.value
+    if(email == "" | !regex.signupEmail.value.test(email)){
+        dangerClass(regex.signupEmail.invalidEmailMsg)
+        return
+    }
+    if(password == ""| !regex.signupPassword.value.test(password)){
+        dangerClass(regex.signupPassword.invalidPasswordMsg)
+        return
+    }
+
+    checkUserEmailInDatabase(email)
+    if(name != "" && email !="" && password !=""){
+        if(correctSignUp){
+            var user = {
+                id: users.length,
+                name: name,
+                email: email,
+                password: password
+            }
+            users.push(user);
+            successClass("Successfully Created")
+            updateLocalStorage();
+            clearForm();
+            correctSignUp = false;
+        }else{
+            warningClass(regex.signupEmail.emailExistMsg)
         }
-        console.log(user);
-        users.push(user);
-        successClass("Successfully Created")
-        updateLocalStorage();
-        clearSignUp();
     }else{
-        console.log("invalid");
         dangerClass("Invalid Information")
     }
 }
 
-
 function login(){
-    let UserInfo = checkUserEmailInDatabase(loginEmail.value)
-    if (UserInfo == undefined){
-        console.log(regex.email.emailDoesNotExistMsg);
-        warningClass(regex.email.emailDoesNotExistMsg)
+    let email = loginEmail.value;
+    let password = loginPassword.value;
+    if(email == "" | !regex.signupEmail.value.test(email)){
+        dangerClass(regex.signupEmail.invalidEmailMsg)
+        return
+    }
+    if(password == ""){
+        dangerClass(regex.signupPassword.wrongPasswordMsg)
+        return
+    }
+
+    checkUserEmailInDatabase(email)
+    if (userInfo == undefined){
+        warningClass(regex.signupEmail.emailDoesNotExistMsg)
     }else{
-        if(checkUserPasswordInDb(UserInfo)){
-            console.log("login Successfully");
+        if(checkUserPasswordInDb(userInfo)){
             successClass("login Successfully")
-            welcomePage(UserInfo.name)
-            sessionStorage.setItem('sessionUsername', UserInfo.name)
+            welcomePage(userInfo.name)
+            sessionStorage.setItem('sessionUsername', userInfo.name)
         }
         else{
-            console.log(regex.password.wrongPasswordMsg);
-            dangerClass(regex.password.wrongPasswordMsg)
+            dangerClass(regex.signupPassword.wrongPasswordMsg)
         }
     }
 }
 
 function validateUserInput(element){
-    if(regex[element.id].test(element.value)==true){
-        console.log("match");
-        correct = true;
+    if(regex[element.id].value.test(element.value)==true && element.value != ""){
+        correctSignUp = true;
         element.classList.add("is-valid");
         element.classList.remove("is-invalid");
-        element.nextElementSiblings.classList.add("d-none")
     }else{
-        console.log("not match");
-        correct = false;
+        correctSignUp = false;
         element.classList.add("is-invalid");
         element.classList.remove("is-valid");
-        element.nextElementSiblings.classList.remove("d-none");
     }
 }
 
